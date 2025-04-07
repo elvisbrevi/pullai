b713fa2670d5d296a33848332763054b438b9d63
Elvis Brevi <elvisbrevi@gmail.com>
2025-04-07 00:37:55 -0400

    aborts

--- app/ai-provider/ollama.ts
+++ app/ai-provider/ollama.ts
@@ -10,7 +10,7 @@ async function formatContentWithAI(rawDiff: string, language: string) {
   const prompt = getPrompt(rawDiff, language);
 
   const response = await ollama.chat({
-    model: "gemma3:1b",
+    model: "gemma3:4b",
     messages: [
       {
         role: "user",
@@ -27,7 +27,7 @@ function getPrompt(rawDiff: string, language: string) {
     language === "en" ? "English" : "Spanish"
   } based on the provided raw Git diff. The description should be in **Markdown format** but **must not be wrapped in code blocks** (e.g., \`\`\`markdown, \`\`\`).  
 
-The description should include the following sections structure:  
+The structure of the markdown document should include only the following sections in the mentioned order and language, not more:  
 
 - **Description**: A high-level summary of the changes made. This section **must include a clear and detailed explanation of the modifications, improvements, or new functionalities introduced**. The overall summary should be **integrated into this section, not placed separately at the end**.  
 - **Added Files**: A list of newly added files.  
@@ -35,8 +35,10 @@ The description should include the following sections structure:
 - **Deleted Files**: A list of deleted files.  
 - **Change Details**: A breakdown of the changes made to each added or modified file. **Ensure that changes affecting business logic, API integrations (e.g., SOAP to REST migrations), and system architecture are explicitly mentioned and analyzed in depth.**  
 
-🚨 **Important**:  
-- Do **not** add an additional summary at the end of the document.  
+Important: 
+- Just like the previous five sections, the **Change Details** section should be integrated into the overall summary and not placed separately at the end.
+- Do **not** add an additional summary at the end or at begin of the document.  
+- The markdown format should be consistent throughout the document.
 - Ensure that all relevant changes, including API modifications, business logic updates, and architectural improvements, are properly identified and described.
 - If a major refactor is detected, explain how it improves performance, maintainability, or security.
 - Titles must have the special markdown format (e.g., # Title).
